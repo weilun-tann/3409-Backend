d9c31dd325f8b09eca3145bb3725ba7ea53a73c3
Tan Wei Lun <weilunn.97@gmail.com>
2022-03-23 18:27:07 +0800

    added back/frontend template

--- app.py
+++ app.py
@@ -1,49 +1,133 @@
-#!/usr/bin/env python
-# coding: utf-8
+import os
 
-# In[15]:
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from flask import (Flask, jsonify, redirect, render_template, request,
+                   send_from_directory)
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+from models import cataract, pneumonia
+from schema import (PredictCataractResponseSchema,
+                    PredictPneumoniaResponseSchema)
+
+app = Flask(__name__, template_folder="swagger/templates")
+CORS(app)
+app.config["UPLOAD_FOLDER"] = "uploads"
+
+spec = APISpec(
+    title="AI Doctor Swagger Docs",
+    version="1.0.0",
+    openapi_version="3.0.2",
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
+
+@app.before_request
+def before_request():
+    if request.is_secure:
+        url = request.url.replace('https://', 'http://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+@app.route("/")
+def root():
+    return redirect("/docs", code=302)
 
 
-from flask import Flask
+@app.route("/api/swagger.json")
+def create_swagger_spec():
+    return jsonify(spec.to_dict())
 
 
-# In[19]:
+# GET endpoint with URL parameters
+@app.route("/predict/cataract")
+def predict_cataract():
+    # TODO - fill in your docstrings (make sure name and data type are correct)
+    # Syntax follows OpenAPI3 (aka Swagger)
+    # https://support.smartbear.com/swaggerhub/docs/tutorials/openapi-3-tutorial.html
+    """
+    ---
+    get:
+        summary: Predict cataract based on numerical/categorical variables
+        parameters:
+        - name: age
+          in: query
+          description: The patient's age
+          schema:
+            type: integer
+        - name: gender
+          in: query
+          description: The patient's gender
+          schema:
+            type: string
+        responses:
+            200:
+                content:
+                    application/json:
+                        schema: PredictCataractResponseSchema
+
+    """
+    age = request.args.get("age")
+    gender = request.args.get("gender")
+    res = cataract.predict(age, gender)
+
+    # TODO - If an empty response is returned, `res` and your schema have probably diverged
+    return PredictCataractResponseSchema().dump(res)
 
 
-app = Flask(__name__)
+# GET endpoint with URL parameters
+@app.route("/predict/pneumonia", methods=["POST"])
+def predict_pneumonia():
+    # TODO - fill in your docstrings (make sure name and data type are correct)
+    # Syntax follows OpenAPI3 (aka Swagger)
+    # https://support.smartbear.com/swaggerhub/docs/tutorials/openapi-3-tutorial.html
+    """
+    ---
+    post:
+      summary: Predict pneumonia based on numerical/categorical variables
+      requestBody:
+        content:
+          multipart/form-data:
+            schema:
+              type: object
+              properties:
+                file:
+                  type: string
+                  format: binary
+      responses:
+            200:
+                content:
+                    application/json:
+                        schema: PredictPneumoniaResponseSchema
+
+    """
+    file = request.files["file"]
+    filename = secure_filename(file.filename)
+    path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    file.save(path)
+    res = pneumonia.predict(path)
+
+    # TODO - If an empty response is returned, `res` and your schema have probably diverged
+    return PredictPneumoniaResponseSchema().dump(res)
 
 
-# In[20]:
+# TODO - for each endpoint you add, add the corresponding function here
+with app.test_request_context():
+    spec.path(view=predict_cataract)
+    spec.path(view=predict_pneumonia)
 
 
-from flask import request, render_template
-from keras.models import load_model
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method=="POST":
-        NPTA = request.form.get("NPTA")
-        TLTA = request.form.get("TLTA")
-        WCTA = request.form.get("WCTA")
-        print(NPTA, TLTA, WCTA)
-        model = load_model("BKRNN")
-        pred = model.predict([[float(NPTA), float(TLTA), float(WCTA)]])
-        print(pred)
-        s = "The predicted bankruptcy score is : " + str(pred)
-        return(render_template("index.html", result=s))
+@app.route("/docs")
+@app.route("/docs/<path:path>")
+def swagger_docs(path=None):
+    if not path or path == "index.html":
+        root_url = request.url.replace("/docs", "")
+        print(f"root_url: {root_url}")
+        return render_template("index.html", base_url="/docs", root_url={root_url})
     else:
-        return(render_template("index.html", result="2")) 
-
-
-# In[ ]:
+        return send_from_directory("./swagger/static", path)
 
 
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
-
-
-
-
---
+++ models/cataract.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.append("..") 
+from schema import PredictCataractResponseSchema
+
+"""
+TODO - ADD THE ARGUMENTS YOU NEED FOR YOUR MODEL, LEAVING TYPES AS STRING 
+1. All arguments will be passed as string
+2. The return value should be a dictionary, do NOT change the return type
+3. Backend will simply send it back as JSON
+"""
+def predict(age: str, gender: str) -> PredictCataractResponseSchema:
+    # casting
+    # load model
+    # model.predict
+    # return results
+
+    # TODO - `res` should exactly match whatever schema you use for .dump(res) below
+    # TODO - Ensure the key NAMES are identical to PredictCataractResponseSchema
+    # TODO - Ensure the value TYPES are identical to PredictCataractResponseSchema
+    res: PredictCataractResponseSchema = {
+        "cataract_type": "nuclear",
+    }
+    return res
