3c7413af08d5b28413758a3a207a26c1c62d451b
autoblack <cclauss@users.noreply.github.com>
2022-04-04 09:22:08 +0000

    fixup: Format Python code with Black

--- schema.py
+++ schema.py
@@ -17,11 +17,6 @@ class PredictPneumoniaResponseSchema(Schema):
     probability = fields.Float()
     outcome = fields.Int()
 
-class PredictCataractResponseSchema(Schema):
-    outcome = fields.String()
-
-class PredictRespiratoryResponseSchema(Schema):
-    outcome = fields.String()
 
 class PredictCataractResponseSchema(Schema):
     outcome = fields.String()
