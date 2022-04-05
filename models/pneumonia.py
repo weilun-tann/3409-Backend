ba1c42bfdaa67971fd979bccbcf6c51fd3ccde25
Yin Wee <wee640@gmail.com>
2022-04-05 16:55:19 +0800

    Updated pneumonia model

--- models/pneumonia.py
+++ models/pneumonia.py
@@ -1,7 +1,12 @@
+from schema import PredictPneumoniaResponseSchema
 import sys
+from keras.models import model_from_json
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from keras.preprocessing import image
 
 sys.path.append("..")
-from schema import PredictPneumoniaResponseSchema
 
 """
 TODO - ADD THE ARGUMENTS YOU NEED FOR YOUR MODEL, LEAVING TYPES AS STRING 
@@ -22,12 +27,27 @@ def predict(absolute_image_path: str) -> PredictPneumoniaResponseSchema:
     """
     # casting
     # load model
+    model = model_from_json(
+        open("models/weights/pneumonia/pneumonia.json").read())
+    model.load_weights("models/weights/pneumonia/pneumonia.h5")
+
     # model.predict
-    # return results
-    print(f"absolute_image_path: {absolute_image_path}")
+    img_dims = 64
+    img = absolute_image_path
+    img = cv2.resize(img, (img_dims, img_dims))
+    img = np.dstack([img, img, img])
+    img = img.astype('float32') / 255
+    result = model.predict(np.expand_dims(image.img_to_array(img), axis=0))
+
+    if result[0][0] > 0.5:
+        prediction = 'Present'
+    else:
+        prediction = 'Absent'
 
     # TODO - `res` should exactly match whatever schema you use for .dump(res) below
     # TODO - Ensure the key NAMES are identical to PredictPneumoniaResponseSchema
     # TODO - Ensure the value TYPES are identical to PredictPneumoniaResponseSchema
-    res: PredictPneumoniaResponseSchema = {"probability": 0.5, "outcome": 1}
+    res: PredictPneumoniaResponseSchema = {
+        "outcome": prediction
+    }
     return res
